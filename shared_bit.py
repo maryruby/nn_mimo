62f9d46f3db70d03f9e8e257b63cb18e69ae4fc7
Andrey Danilchenko <danilchenko.andrey@gmail.com>
2017-09-28 09:13:03 +0300

    restoring model, use batches generator

--- shared_bit.py
+++ shared_bit.py
@@ -7,7 +7,6 @@ import tensorflow as tf
 import time
 
 import utils
-import quality
 
 
 logger = logging.getLogger(__name__)
@@ -41,8 +40,10 @@ def create_model(x, args):
 # logits = predicted tensor, labels = ideal tensor;
 def create_loss(logits, labels):
     with tf.name_scope('cross_entropy'):
-        cross_entropy = tf.reduce_mean(
-            tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, logits=logits), axis=1))
+        ce = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, logits=logits), axis=0)
+        for column in xrange(utils.N_BITS):
+            tf.summary.scalar('ce_%d' % column, ce[column])
+        cross_entropy = tf.reduce_sum(ce)
     tf.summary.scalar('cross_entropy', cross_entropy)
     return cross_entropy
 
@@ -51,7 +52,7 @@ def binary_error_rate(logits, labels, threshold=0.5):
     t = tf.constant(threshold)
     with tf.name_scope('ber'):
         with tf.name_scope('errors'):
-            errors = tf.not_equal(tf.greater_equal(logits, t), labels)
+            errors = tf.not_equal(tf.cast(tf.greater_equal(logits, t), tf.float32), labels)
         with tf.name_scope('column_ber'):
             column_ber = tf.reduce_mean(tf.cast(errors, tf.float32), axis=0)
             for column in xrange(utils.N_BITS):
@@ -71,7 +72,16 @@ def training(loss, args):
         # Use the optimizer to apply the gradients that minimize the loss
         # (and also increment the global step counter) as a single training step
         train_op = optimizer.minimize(loss, global_step=global_step)
-        return train_op
+        return train_op, global_step
+
+
+def prepare_writers(sess, args):
+    if tf.gfile.Exists(args.log_dir):
+        tf.gfile.DeleteRecursively(args.log_dir)
+    tf.gfile.MakeDirs(args.log_dir)
+    train_writer = tf.summary.FileWriter(args.log_dir + '/train', sess.graph)
+    test_writer = tf.summary.FileWriter(args.log_dir + '/test')
+    return train_writer, test_writer
 
 
 def main(args):
@@ -85,7 +95,7 @@ def main(args):
     logits = create_model(x_, args)
     loss = create_loss(logits, y_)
     cber, ber = binary_error_rate(logits, y_)
-    train_op = training(loss, args)
+    train_op, global_step = training(loss, args)
 
     # Here is what my boss think I do:
 
@@ -105,11 +115,7 @@ def main(args):
 
     saver = tf.train.Saver()
     merged = tf.summary.merge_all()
-    if tf.gfile.Exists(args.log_dir):
-        tf.gfile.DeleteRecursively(args.log_dir)
-    tf.gfile.MakeDirs(args.log_dir)
-    train_writer = tf.summary.FileWriter(args.log_dir + '/train', sess.graph)
-    test_writer = tf.summary.FileWriter(args.log_dir + '/test')
+    train_writer, test_writer = prepare_writers(sess, args)
 
     logger.info('Reading train data...')
     X_train, Y_train = utils.read_data('data/1/y_big_test.csv', 'data/1/b_big_test.csv')
@@ -120,36 +126,36 @@ def main(args):
     logger.info('Initialize model...')
     sess.run(init_op)
 
-    logger.info('Training...')
     min_iterations = 0
-    max_iterations = args.max_iterations
-    batch_size = args.batch_size
-    threshold = 0.5
+    if args.min_epoch > 0:
+        min_iterations = args.min_epoch
+        model_filename = '%s-%s' % (args.model_filename, args.min_epoch)
+        logger.info('Restore model of %d epoch from %s', args.min_epoch, model_filename)
+        saver.restore(sess, model_filename)
+    
+    max_iterations = args.max_epochs
+    global_iteration = 0
+    logger.info('Training...')
     for epoch in xrange(min_iterations, max_iterations):
         start_time = time.time()
-        perm = np.random.permutation(X_train.shape[0])
-        X_p = X_train[perm, :]
-        Y_p = Y_train[perm, :]
-
-        total_batches = X_train.shape[0] / batch_size
-        for b in xrange(total_batches):
-            batch_x = X_p[batch_size * b:batch_size * (b + 1),:]
-            batch_y = Y_p[batch_size * b:batch_size * (b + 1),:]
-            feed_dict = {x_: batch_x, y_: batch_y}
-            
-            summary, _ = sess.run([merged, train_op],
-                                  feed_dict=feed_dict)
-            train_writer.add_summary(summary, epoch * total_batches + b)
+        
+        for batch_x, batch_y in utils.generate_batches(X_train, Y_train, args.batch_size):
+            if global_iteration % 100 == 99:
+                summary, _, global_iteration = sess.run([merged, train_op, global_step], 
+                                                        feed_dict={x_: batch_x, y_: batch_y})
+                train_writer.add_summary(summary, global_iteration)
+            else:
+                _, global_iteration = sess.run([train_op, global_step], feed_dict={x_: batch_x, y_: batch_y})
         duration = time.time() - start_time
-        logger.debug('Epoch %d done for %.2f secs', epoch, duration)
+        logger.debug('Epoch %d done for %.2f secs (current global iteration: %d)', epoch, duration, global_iteration)
 
         if epoch % 10 == 0:
-            summary, train_cber, train_ber, train_ce = sess.run([merged, cber, ber, loss], {x_: X_train, y_: Y_train})
-            test_writer.add_summary(summary, epoch)
-            logger.info('TRAIN epoch: %d CE: %.5f column BER: [%s] (mean: %.5f)', 
+            train_cber, train_ber, train_ce = sess.run([cber, ber, loss], {x_: X_train, y_: Y_train})
+            logger.info('TRAIN epoch: %d CE: %.5f column BER: [%s] (mean: %.5f)',
                         epoch, train_ce, ','.join('%.5f' % c for c in train_cber), train_ber)
         
-            val_cber, val_ber, val_ce = sess.run([cber, ber, loss], {x_: X_valid, y_: Y_valid})
+            summary, val_cber, val_ber, val_ce = sess.run([merged, cber, ber, loss], {x_: X_valid, y_: Y_valid})
+            test_writer.add_summary(summary, global_iteration)
             logger.info('VALIDATION epoch: %d CE: %.5f column BER: [%s] (mean: %.5f)', 
                         epoch, val_ce, ','.join('%.5f' % c for c in val_cber), val_ber)
             
@@ -166,8 +172,8 @@ def main(args):
     X_test, Y_test = utils.read_data('data/1/y_big_test.csv', 'data/1/b_big_test.csv')
 
     test_cber, test_ber, test_ce = sess.run([cber, ber, loss], {x_: X_test, y_: Y_test})
-    logger.info('TEST epoch: %d CE: %.5f column BER: [%s] (mean: %.5f)', 
-                epoch, test_ce, ','.join('%.5f' % c for c in test_cber), test_ber)
+    logger.info('TEST CE: %.5f column BER: [%s] (mean: %.5f)', 
+                test_ce, ','.join('%.5f' % c for c in test_cber), test_ber)
 
     save_path = saver.save(sess, args.model_filename, global_step = max_iterations)
     logger.info('Model stored in %s' % save_path)
@@ -177,7 +183,8 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
     parser = argparse.ArgumentParser()
     parser.add_argument('--learning-rate', help='Initial learning rate', type=float, default=0.01)
-    parser.add_argument('--max-iterations', help='Number of epochs to run trainer', type=int, default=100)
+    parser.add_argument('--min-epoch', help='Epoch to load model from', type=int, default=0)
+    parser.add_argument('--max-epochs', help='Number of epochs to run trainer', type=int, default=100)
     parser.add_argument('--batch-size', help='Batch size', type=int, default=100)
     parser.add_argument('--n-shared', help='Size of shared hidden layer', type=int, default=32)
     parser.add_argument('--n-hidden', help='Size of separate hidden layer', type=int, default=16)
