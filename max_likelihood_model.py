d49debd174fe848560c8e573a24ee662705be3d3
Andrey Danilchenko <danilchenko.andrey@gmail.com>
2017-11-16 20:24:00 +0300

    max likelihood network

--- max_likelihood_model.py
+++ max_likelihood_model.py
@@ -4,13 +4,32 @@ from model import BaseModel
 import utils
 
 
+def to_binary_tensor(x):
+    if x == 0: 
+        return tf.constant([0] * utils.N_BITS, dtype=tf.int32)
+    bit = []
+    while x:
+        bit.append(x % 2)
+        x >>= 1
+    if len(bit) < utils.N_BITS:
+        bit = bit + [0] * (utils.N_BITS - len(bit))
+    return tf.constant(bit[::-1], dtype=tf.int32)
+
+
+binary_tensors =  tf.stack([to_binary_tensor(x) for x in xrange(2 ** utils.N_BITS)], axis=0)
+
+
 class MaxLikelihoodModel(BaseModel):
     def __init__(self, args, x_input, y_input):
         self.args = args
         self.x_input = x_input
-        self.y_input = y_input
-        self.predictions = create_model(self.x_input, self.args)
-        self.loss = create_loss(self.predictions, self.y_input)
+        y_int = tf.cast(y_input, dtype=tf.int32)
+        digits = tf.reshape(2 ** tf.range(utils.N_BITS - 1, -1, -1, dtype=tf.int32), (utils.N_BITS, 1))
+        self.y_input = tf.one_hot(tf.matmul(y_int, digits), 2**utils.N_BITS)
+
+        logits = create_model(self.x_input, self.args)
+        self.predictions = tf.argmax(logits, 1)
+        self.loss = create_loss(logits, self.y_input)
         self.train_op, self.global_step = training(self.loss, self.args)
 
     def get_predictions(self):
@@ -37,10 +56,8 @@ def create_model(x, args):
                                                                      activation_fn=tf.nn.relu))
 
         with tf.name_scope('softmax'):
-            output_size = self.y_input.get_shape()[1]
-
-            output = tf.contrib.layers.fully_connected(hiddens[-1], output_size,
-                                                       activation_fn=tf.nn.identity)
+            output = tf.contrib.layers.fully_connected(hiddens[-1], 2**utils.N_BITS,
+                                                       activation_fn=tf.identity)
             tf.summary.histogram('activations', output)
         return output
 
